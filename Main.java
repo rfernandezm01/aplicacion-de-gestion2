05874761c47a0eee64ca469234d8e93a3a8bc968
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-07 17:50:27 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Terminal terminal = new Terminal();
+        Controlador controlador = new Controlador();
+
+        terminal.menu();
+    }
+}
