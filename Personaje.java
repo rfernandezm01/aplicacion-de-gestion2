05f6b62c17f0198f55537563d7a4ba91f869f1b8
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-07 17:51:39 +0200

    Create Personaje.java

---
+++ Personaje.java
@@ -0,0 +1,24 @@
+package com.company;
+
+enum Armas{
+    espada,
+    arco,
+    catalizador,
+    claymore,
+    lanza
+}
+public class Personaje {
+    private int perID;
+    private String nombre;
+    private int edad;
+    private Armas armas;
+    private String rol;
+    private String genero;
+    private String nomconstelacion;
+    private String rango;
+    private int nvlper;
+
+    public Personaje(){
+
+    }
+}
