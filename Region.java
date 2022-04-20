8e54e76a1f4f425800fa48838b86836a0acc01fd
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-20 04:56:22 +0200

    Update Region.java

--- Region.java
+++ Region.java
@@ -1,29 +1,32 @@
 package com.company;
-
 import java.util.ArrayList;
 
-enum Elemento{
-    Pyro,
-    Hydro,
-    Electro,
-    Anemo,
-    Cryo,
-    Dendro,
-    Geo
-}
-
-public class Region {
-    private int ID;
-    private String nregion;
-    private String ambientado;
-    private Elemento elemento;
-    private ArrayList<Personaje>personajes;
-
-    public Region(int ID,String nregion, String ambientado, Elemento elemento, ArrayList<Personaje> personajes){
-        this.ID = ID;
-        this.nregion = nregion;
-        this.ambientado = ambientado;
-        this.elemento = elemento;
-        this.personajes = new ArrayList<Personaje>();
+    enum Elemento{
+        Pyro,
+        Hydro,
+        Electro,
+        Anemo,
+        Cryo,
+        Dendro,
+        Geo
     }
-}
+
+    public class Region {
+        private int ID;
+        private String nregion;
+        private String ambientado;
+        private Elemento elemento;
+        private ArrayList<Personaje>personajes;
+
+        public Region(int ID,String nregion, String ambientado, Elemento elemento, ArrayList<Personaje> personajes){
+            this.ID = ID;
+            this.nregion = nregion;
+            this.ambientado = ambientado;
+            this.elemento = elemento;
+            this.personajes = new ArrayList<Personaje>();
+        }
+        public String toString(){
+
+            return toString();
+        }
+    }
