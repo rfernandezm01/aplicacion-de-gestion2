7bf64ddf5f01dcd1bee8d0ad69dc5922f83d020f
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-21 17:51:03 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
         Terminal terminal = new Terminal();
         Controlador controlador = new Controlador();
 
+        terminal.emplenarRegion();
+        controlador.toString();
         terminal.menu();
     }
 }
