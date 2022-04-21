9bf44c6b6dd4e1353b13d776247d3df68607273d
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-21 17:50:22 +0200

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -3,7 +3,15 @@ package com.company;
 import java.util.ArrayList;
 
 public class Controlador {
-    ArrayList<Region>region;
-    ArrayList<Personaje>personaje;
+    private ArrayList<Region>region;
+    private ArrayList<Personaje>personajes;
 
+    public ArrayList<Personaje> getPersonajes() {
+        for (int i = 0; i < personajes.size(); i++) {
+            if(personajes.get(i).getregion()){
+
+            }
+        }
+        return personajes;
+    }
 }
