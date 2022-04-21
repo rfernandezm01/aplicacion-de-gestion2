89afd6735ba8dbc5b1e2cf540f985b7eea5b6c7d
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-21 17:52:02 +0200

    Update Region.java

--- Region.java
+++ Region.java
@@ -1,32 +1,68 @@
 package com.company;
 import java.util.ArrayList;
 
-    enum Elemento{
-        Pyro,
-        Hydro,
-        Electro,
-        Anemo,
-        Cryo,
-        Dendro,
-        Geo
+enum Elemento{
+    Pyro,
+    Hydro,
+    Electro,
+    Anemo,
+    Cryo,
+    Dendro,
+    Geo
+}
+
+enum nRegion{
+    Mondstadt,
+    Liyue,
+    Inazuma,
+    Snezhnaya,
+    Sumeru,
+    Fontaine,
+    Natlan,
+    Khaenriah
+}
+
+public class Region {
+    private int ID;
+    private nRegion nregion;
+    private String ambientado;
+    private Elemento elemento;
+    private ArrayList<Personaje>personajes;
+
+    public Region(int ID,nRegion nregion, String ambientado, Elemento elemento, ArrayList<Personaje> personajes){
+        this.ID = ID;
+        this.nregion = nregion;
+        this.ambientado = ambientado;
+        this.elemento = elemento;
+        this.personajes = new ArrayList<Personaje>();
     }
 
-    public class Region {
-        private int ID;
-        private String nregion;
-        private String ambientado;
-        private Elemento elemento;
-        private ArrayList<Personaje>personajes;
-
-        public Region(int ID,String nregion, String ambientado, Elemento elemento, ArrayList<Personaje> personajes){
-            this.ID = ID;
-            this.nregion = nregion;
-            this.ambientado = ambientado;
-            this.elemento = elemento;
-            this.personajes = new ArrayList<Personaje>();
+    public Elemento comprobarelemento(){
+        if(this.elemento == Elemento.Pyro || this.elemento == Elemento.Hydro || this.elemento == Elemento.Electro || this.elemento == Elemento.Anemo || this.elemento == Elemento.Cryo || this.elemento == Elemento.Dendro || this.elemento == Elemento.Geo){
+            this.elemento = this.elemento;
+        }else{
+            this.elemento = Elemento.Pyro;
         }
-        public String toString(){
-
-            return toString();
-        }
+        return comprobarelemento();
     }
+    public String comprobarnRegion(){
+        if(this.nregion == nRegion.Mondstadt || this.nregion == nRegion.Liyue || this.nregion == nRegion.Inazuma || this.nregion == nRegion.Sumeru || this.nregion == nRegion.Fontaine || this.nregion == nRegion.Natlan || this.nregion == nRegion.Snezhnaya || this.nregion == nRegion.Khaenriah){
+            this.nregion = this.nregion;
+        }else{
+            this.nregion = nRegion.Mondstadt;
+        }
+        return comprobarnRegion();
+    }
+
+    public nRegion getNregion() {
+        return nregion;
+    }
+    public Elemento getElemento(){
+        return elemento;
+    }
+
+    public String toString(){
+
+        return toString();
+    }
+}
