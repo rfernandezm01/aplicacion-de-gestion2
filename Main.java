eddb05ab18dac8309c56191058e21acb972ec2b0
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-27 07:50:01 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,22 @@
 package com.company;
 
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         Terminal terminal = new Terminal();
         Controlador controlador = new Controlador();
+        Region region = new Region(1,nRegion.Mondstadt,"Alemania", Elemento.Anemo, new ArrayList<>());
+        Controlador alta = new Controlador();
 
         terminal.emplenarRegion();
         controlador.toString();
         terminal.menu();
     }
 }
+
