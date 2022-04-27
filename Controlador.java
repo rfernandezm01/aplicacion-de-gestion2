d89292e77bce94a1d147a70ae92cb4311f133e50
rfernandezm01 <81315987+rfernandezm01@users.noreply.github.com>
2022-04-27 07:49:36 +0200

    Update Controlador.java

--- Controlador.java
+++ Controlador.java
@@ -14,4 +14,30 @@ public class Controlador {
         }
         return personajes;
     }
+    public class alta{
+        private int ID;
+        private nRegion nregion;
+        private String ambientado;
+        private Elemento elemento;
+        private ArrayList<Personaje>personajes;
+
+        public int getid(){
+            return ID;
+        }
+        public nRegion getnregion(){
+            return nregion;
+        }
+        public String getAmbientado(){
+            return ambientado;
+        }
+        public Elemento getElemento(){
+            return elemento;
+        }
+        public void setid(int ID, nRegion nregion, String ambientado, Elemento elemento){
+            this.ID = ID;
+            this.nregion = nregion;
+            this.ambientado = ambientado;
+            this.elemento = elemento;
+        }
+    }
 }
